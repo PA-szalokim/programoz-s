80dae2a8ba7649dd73f5ac0e89bdce3fa00532a1
pa-szalokim <112852314+PA-szalokim@users.noreply.github.com>
2022-09-26 08:17:12 +0200

    Add files via upload

---
+++ változók.py
@@ -0,0 +1,17 @@
+import os
+x = 5
+y = "John"
+print(x)
+print (type(x))
+print(y)
+print(type(y))
+x = 6.2
+print(x)
+print(type(x))
+
+x = 4 # x is of type int
+print(x)
+print(type(x))
+x = "Sally" # is now of type str
+print(x)
+print(type(x)) 
