15488ee34289a66d6ee7d5d06882e453d4fa0356
pa-szalokim <112852314+PA-szalokim@users.noreply.github.com>
2022-10-10 09:39:59 +0200

    Add files via upload

---
+++ 11_12ora.py
@@ -0,0 +1,24 @@
+"""
+gondolt_szam = 4
+print(type(gondolt_szam))
+tipp = input('gondoltam egy szamra.Tippeld meg ' )
+tipp = int(tipp)
+if tipp == gondolt_szam:
+  print('Ügyes vagy ')
+if tipp > gondolt_szam :
+    print(' tipp >  mint a gondolt szam')
+if tipp < gondolt_szam :
+    print('tipp < mint a gondolt szam')
+print( 'Pápá ')
+"""
+
+
+gondolt_szam= 4
+tipp = input('gondoltam egy szamra.Tippeld meg ' )
+tipp= int(tipp)
+if tipp == gondolt_szam:
+  print('Ügyes vagy ')
+else: print(Nem talált)
+print( 'Pápá ')
+      
+
