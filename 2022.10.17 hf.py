4e5c9d596952bd07a2141f5611b91bbea2a3c7cc
pa-szalokim <112852314+PA-szalokim@users.noreply.github.com>
2022-10-17 09:14:49 +0200

    Add files via upload

---
+++ 2022.10.17 hf.py
@@ -0,0 +1,13 @@
+jegy=input('kerek egy osztályzatot:')
+if jegy == "1":
+    print(' elegtelen')
+elif jegy == "2":
+    print(' elegseges')
+elif jegy == "3":
+    print(' kozepes')
+elif jegy == "4":
+    print(' jo')
+elif jegy == "5":
+    print('jeles')
+else:
+    print(' nem ervenyes jegyet adtal meg')    
