47725e65b6b10bd5d8f2c85b4b31724f3481171e
pa-szalokim <112852314+PA-szalokim@users.noreply.github.com>
2022-10-24 09:41:57 +0200

    Add files via upload

---
+++ 2022.10.24.py
@@ -0,0 +1,49 @@
+"""
+szam=0
+for x in "banana":
+    print(szam,x,sep="===", end='\n')
+    szam = szam +1
+
+
+a = "hello,world"
+print(len(a))
+
+
+txt = "The best things in life are free!"
+if "freee" in txt:
+print("yes, 'free' is present.")
+ else:
+     print("no,'free' is present.") 
+
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+b = "Hello, World!"
+
+print(b[1:5])  # llo
+
+c = "H"+b [1.5]
+print (c)
+
+b = "Hello, World!"
+
+print(b[-5:-2]) # orl
+
+a = "Hello, World!"
+
+print(a.upper())
+
+
+a = "Hello, World!"
+
+print(a.lower())
+
+a = " Hello, World! "
+
+print(a)
+
+print(a.strip()) # returns "Hello, World!"
+szam = 10 
+"""
+
