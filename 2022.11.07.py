153198c8050cda1d4014186d5e8e24eec768ef9f
pa-szalokim <112852314+PA-szalokim@users.noreply.github.com>
2022-11-14 09:18:51 +0100

    Add files via upload

---
+++ 2022.11.07.py
@@ -0,0 +1,122 @@
+"""
+szam=0
+for x in "banana":
+    print(szam,x,sep="===", end='\n')
+    szam = szam +1
+
+
+a = "hello,world"
+print(len(a))
+
+
+txt = "The best things in life are free!"
+if "freee" in txt:
+print("yes, 'free' is present.")
+ else:
+     print("no,'free' is present.") 
+
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+b = "Hello, World!"
+
+print(b[1:5])  # llo
+
+c = "H"+b [1.5]
+print (c)
+
+b = "Hello, World!"
+
+print(b[-5:-2]) # orl
+
+a = "Hello, World!"
+
+print(a.upper())
+
+
+a = "Hello, World!"
+
+print(a.lower())
+
+a = " Hello, World! "
+
+print(a)
+
+print(a.strip()) # returns "Hello, World!"
+szam = 10 
+
+
+
+a = " Hello, World! "
+
+print(a.strip()) # returns "Hello, World!"
+
+
+a = "Hello, World!"
+
+print(a.replace("HHHH", "J"))
+
+a = "Hello, World!, Hello, Hello, "
+print(a)
+uj_string = a.replace(" Hello, World!, hello   hello" , "xxxx")
+print ("a regi string"+ "   " +a)
+print(" a regi string" ,a, sep = "  " )
+print (" az uj " + uj_string)
+
+a = "Hello"
+
+b = "World"
+
+space = " "
+
+felkialtojel = "!"
+
+c = a + space + b +felkialtojel + space + a + space + b + felkialtojel
+
+print(c)
+
+
+
+a = "Hello"
+
+b = "World"
+
+c = a + " " + b
+
+print(c)
+
+
+
+quantity = 3
+
+itemno = 567
+
+price = 49.95
+
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+
+print(myorder.format(quantity, itemno, price))
+
+
+quantity = 3
+
+itemno = 567
+
+price = 49.95
+
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+
+print(myorder.format(quantity, itemno, price))
+
+
+
+print(" 10d osztaly.1csoport" , end= '\n')
+print(" 10d osztaly.1csoport" , end= '\t\t\t')
+print(" 10d osztaly.1csoport")
+
+"""
+
+
+b= 'hello ,world'
+print()
