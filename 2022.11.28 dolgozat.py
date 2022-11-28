33646c4d3ebf3db10be11941319b99101581795e
pa-szalokim <112852314+PA-szalokim@users.noreply.github.com>
2022-11-28 09:42:22 +0100

    Add files via upload

---
+++ 2022.11.28 dolgozat.py
@@ -0,0 +1,29 @@
+"""
+for _ szam in range(50,101,3):
+    print( szam,end= " ")
+
+for szam in range(50,101,3):
+    if szam % 2 == 0:
+      print(szam, end= " ")
+
+
+szoveg= " Hello vilag"
+for kar in szoveg:
+    print(kar,end= "")
+
+
+szoveg = input("kerem a nevedet : ")
+for kar in szoveg:
+    print(kar.upper(),end= "")
+"""
+szamlalo=0
+nev = input("kerem a  teljesa nevedet kisbetuvel : ")
+for kar in nev:
+  if szamlalo== 0
+    print(kar, end="")
+else:
+    
+     print(szamlalo, end="")   
+    szamlalo +=1 
+
+
