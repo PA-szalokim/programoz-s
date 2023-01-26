6bdb90548cfa941f72042ebfc27fa81c770c3fd6
pa-szalokim <112852314+PA-szalokim@users.noreply.github.com>
2023-01-26 08:42:41 +0100

    Add files via upload

---
+++ 37-38óra.py
@@ -0,0 +1,39 @@
+#Eljaras
+"""
+def menut_kiir():
+    '''
+    A menu megjelenitese a kepernyon
+    '''
+    print('1. Uj adatbevitele')
+    print('2. Adatok modositasa/ torlese')
+    print('3. Kilepes a programbol')
+
+menut_kiir()
+
+
+
+def menut_kiir(tipus):
+    '''
+    A menu megjelenitese a kepernyon
+    '''
+    if tipus == 2:
+        print('1. Uj adatbevitele')
+        print('3. Kilepes a programbol')
+
+    if tipus == 3:
+        print('1. Uj adatbevitele')
+        print('2. Adatok modositasa/ torlese')
+        print('3. Kilepes a programbol')
+# eljaras hivasa
+menut_kiir(3)
+
+ """
+
+def koszont():
+     print(' Udv a fedelzeten!')
+
+def koszont_nevvel(nev):
+    print('Szia'+ nev + ',Udv a fedelzeten!')
+
+koszont_nevvel( Marci)    
+        
