cbd2849ae816d3d182e5d45d4e7ddd3f04ae3c8b
pa-szalokim <112852314+PA-szalokim@users.noreply.github.com>
2023-05-25 08:01:32 +0200

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,6 @@
+def osszeg(a,b):
+    return a + b
+
+def kivonas(szam1,szam2):
+    eredmeny = szam1 - szam2
+    return eredmeny
---
+++ program1.py
@@ -0,0 +1,4 @@
+import module1
+#print(module1.osszeg(1,4))
+
+print(module1.osszeg(5,10))
